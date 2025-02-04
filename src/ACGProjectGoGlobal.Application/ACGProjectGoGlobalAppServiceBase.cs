24487b3d0798b89733b321cb5c8229ab66d7f01e
bene-coder <onyiriaghabenedict@gmail.com>
2025-02-04 08:32:43 +0100

    First commit to the project

---
+++ src/ACGProjectGoGlobal.Application/ACGProjectGoGlobalAppServiceBase.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services;
+
+namespace ACGProjectGoGlobal
+{
+    /// <summary>
+    /// Derive your application services from this class.
+    /// How do i go about it
+    /// </summary>
+    public abstract class ACGProjectGoGlobalAppServiceBase : ApplicationService
+    {
+        protected ACGProjectGoGlobalAppServiceBase()
+        {
+            LocalizationSourceName = ACGProjectGoGlobalConsts.LocalizationSourceName;
+        }
+    }
+}
